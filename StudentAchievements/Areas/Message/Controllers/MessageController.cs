52e22e19352c8d3ce3b9935066fa0f678441a288
Zan <sochnev66@gmail.com>
2021-03-05 17:41:15 +0300

    Edit messenger

--- StudentAchievements/Areas/Message/Controllers/MessageController.cs
+++ StudentAchievements/Areas/Message/Controllers/MessageController.cs
@@ -5,7 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 using StudentAchievements.Areas.Message.Models.ViewModels;
+using StudentAchievements.Models;
+using StudentAchievements.Areas.Authorization.Models;
 
 namespace StudentAchievements.Areas.Message.Controllers
 {
@@ -13,16 +16,73 @@ namespace StudentAchievements.Areas.Message.Controllers
     [Authorize(Roles = "Employer, Student")]
     public class MessageController : Controller
     {
-        public IActionResult Index() => View(new MessageListViewModel());
+        private StudentAchievementsDbContext context;
+        private IUserRepository userRepository;
+
+        public MessageController(StudentAchievementsDbContext _context, IUserRepository _userRepository)
+        {
+            context = _context;
+            userRepository = _userRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var currentUser = await userRepository.Users.FirstOrDefaultAsync(u => u.Email == User.Identity.Name);
+
+            var viewModel = new List<MessageListViewModel>();
+
+            var chats = context.Chats.Include(m => m.Messages)
+                                    .Include(o => o.OneUser)
+                                    .Include(t => t.TwoUser)
+                                    .Where(p => p.OneUser == currentUser || p.TwoUser == currentUser)
+                                    .ToList();
+
+            foreach (var chat in chats)
+            {
+                viewModel.Add(new MessageListViewModel()
+                {
+                    Me = currentUser,
+                    Companion = chat.OneUser != currentUser ? chat.OneUser : chat.TwoUser,
+                    Chat = chat,
+                    UnreadMessageCount = chat.Messages.Where(p => p.IsViewed == false && p.Sender != currentUser.Name).Count()
+                });
+            }
+
+            return View(viewModel);
+        }
 
         public IActionResult Back()
         {
-            if(User.IsInRole("Employer"))
+            if (User.IsInRole("Employer"))
             {
                 return RedirectToAction("Index", "Employer", new { area = "Employer" });
             }
 
             return RedirectToAction("Index", "Student", new { area = "Student" });
         }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMessages(string id)
+        {
+            var currentUser = await userRepository.Users.FirstOrDefaultAsync(u => u.Email == User.Identity.Name);
+
+            var companion = await userRepository.Users.FirstOrDefaultAsync(u => u.Id == id);
+
+            var chat = await context.Chats.Include(o => o.OneUser)
+                                        .Include(t => t.TwoUser)
+                                        .Include(m => m.Messages)
+                                        .FirstOrDefaultAsync(p => (p.OneUser == currentUser || p.OneUser == companion) && (p.TwoUser == currentUser || p.TwoUser == companion));
+
+            var model = new MessageListViewModel()
+            {
+                Me = currentUser,
+                Companion = companion,
+                Chat = chat
+            };
+
+            return PartialView("Messages", model);
+        }
+
+        
     }
 }
--- StudentAchievements/Areas/Message/Models/ViewModels/MessageListViewModel.cs
+++ StudentAchievements/Areas/Message/Models/ViewModels/MessageListViewModel.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using StudentAchievements.Areas.Authorization.Models;
 
 namespace StudentAchievements.Areas.Message.Models.ViewModels
 {
     public class MessageListViewModel
     {
-        public string Layout { get;set; }
+        public User Me { get;set; }
+        public User Companion { get;set; }
+        public Chat Chat { get;set; }
+        public int UnreadMessageCount { get;set; }
     }
 }
